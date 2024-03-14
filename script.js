e52fd152b4d28862e39521fc6e4d345dba0f0554
Mola Dafa <mola_d@hotmail.com>
2024-03-15 04:10:28 +1100

    shapes printing to screen

--- script.js
+++ script.js
@@ -1,21 +1,76 @@
+// const { writeFile } = require('fs/promises');
+$(document).ready(function() {
+
 class Shape {
 constructor ( colour, text){
         this.colour = colour;
-        this.text = text;
-
-        renderCanvas(){
-            var imageArea = $("<svg>");
-            imageArea.attr({"version":"1.1",
-            "width":"300",
-            "height":"200",
-            "xmlns":"http://www.w3.org/2000/svg"});
-        }
+        this.text = text;             
+}
+renderText(){
+    var createtext = $("<text>");
+    createtext.attr({"x":"150",
+    "y":"125",
+    "font-size":"60",
+    "text-anchor":"middle",
+    "fill":"white"});
+    createtext.text(this.text);
 }
 }
 
-class Circle extends Shape{
-    render (){
-        var doc = document.$("body");
+class Render{
+    render(){
+        var imageArea = $("<svg>");
+        imageArea.attr({"version":"1.1",
+        "width":"300",
+        "height":"200",
+        "xmlns":"http://www.w3.org/2000/svg",
+        "id":"print-area"});
+        $("body").append(imageArea);
 
     }
 }
+
+class Circle extends Shape{
+    renderShape (){
+        //var doc = document.$("body");
+        var circle = $("<circle>");
+        circle.attr({"cx":"150",
+        "cy":"100",
+        "r":"80",
+        "fill":`${this.colour}`});
+        $("#print-area").append(circle);
+    }
+
+}
+
+class Square extends Shape{
+    renderShape (){
+        //var doc = document.$("body");
+        var circle = $("<square>");
+        circle.attr({"x":"150",
+        "y":"100",
+        "width":"80",
+        "fill":`${this.colour}`});
+    }
+
+}
+
+class Triangle extends Shape{
+    renderShape (){
+        //var doc = document.$("body");
+        var circle = $("<polygon>");
+        circle.attr({"points":"150,50 50,150 250,150",
+        fill:`${this.colour}`});
+    }
+
+}
+
+var canvasArea = new Render();
+
+canvasArea.render();
+
+var newCircle = new Circle("green", "test");
+
+newCircle.renderShape();
+
+});
